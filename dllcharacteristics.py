0893590caf2206d0605a2a1e85195748029295e6
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-23 05:21:42 +0800

    Added more values.

--- dllcharacteristics.py
+++ dllcharacteristics.py
@@ -3,17 +3,41 @@
 import pefile
 from argparse import ArgumentParser
 
+HIGH_ENTROPY_VA = 0x0020
 DYNAMIC_BASE = 0x0040
 FORCE_INTEGRITY = 0x0080
 NX_COMPAT = 0x0100
+NO_ISOLATION = 0x0200
+NO_SEH = 0x0400
+NO_BIND = 0x0800
+APPCONTAINER = 0x1000
+WDM_DRIVER = 0x2000
+GUARD_CF = 0x4000
+TERMINAL_SERVER_AWARE = 0x8000
 
 def get_characteristic_by_value(value):
-    if value == 0x0040:
+    if value == 0x0020:
+        return 'HIGH_ENTROPY_VA'
+    elif value == 0x0040:
         return 'DYNAMIC_BASE'
     elif value == 0x0080:
         return 'FORCE_INTEGRITY'
     elif value == 0x0100:
         return 'NX_COMPAT'
+    elif value == 0x0200:
+        return 'NO_ISOLATION'
+    elif value == 0x0400:
+        return 'NO_SEH'
+    elif value == 0x0800:
+        return 'NO_BIND'
+    elif value == 0x1000:
+        return 'APPCONTAINER'
+    elif value == 0x2000:
+        return 'WDM_DRIVER'
+    elif value == 0x4000:
+        return 'GUARD_CF'
+    elif value == 0x8000:
+        return 'TERMINAL_SERVER_AWARE'
     
 def get_characteristic(pe, char_value):
     status = 'OFF'
